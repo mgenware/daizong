b122b5aa8b5289d608a85ef220e421d43cdb67db
Mgen <m@mgenware.com>
2020-05-24 03:44:52 +0800

    Use more restrictive eslint-typescript rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,16 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: ['plugin:@typescript-eslint/recommended'],
+  extends: ['airbnb-typescript/base', 'plugin:@typescript-eslint/recommended'],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
     project: './tsconfig.json',
   },
   rules: {
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'no-plusplus': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    'no-console': ['error', { allow: ['warn', 'error'] }],
     '@typescript-eslint/switch-exhaustiveness-check': 'error',
   },
 };
