88c3aa09357f5e3c47292393b5c92ef76dcdabfa
Mgen <m@mgenware.com>
2020-05-23 23:59:36 +0800

    Fix failing tests

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -5,13 +5,18 @@ import * as assert from 'assert';
 const execAsync = promisify(exec);
 const confBasic = 'basic';
 
-async function t(configName: string, taskName: string, expected: string) {
+async function t(
+  configName: string,
+  taskName: string,
+  cmd: string,
+  expected: string,
+) {
   const output = await execAsync(
     `node "./dist/main.js" -c "./tests/cfgs/${configName}.js" ${taskName}`,
   );
-  assert.equal(output, expected);
+  assert.equal(output, `>> ${cmd}\n${expected}\n\n`);
 }
 
 it('Single cmd', async () => {
-  await t(confBasic, 'single_cmd', 'hi');
+  await t(confBasic, 'single_cmd', 'echo hi', 'hi');
 });
