0e06cf44bbeb72256d1b7bc990005b55da40d599
Mgen <m@mgenware.com>
2022-01-07 20:51:50 +0800

    Fucking windows issue again

--- tests/dz.test.ts
+++ tests/dz.test.ts
@@ -2,24 +2,28 @@ import { t } from './common.js';
 
 const conf = 'argsConf';
 
-it('No args', async () => {
-  await t(
-    conf,
-    'print',
-    `>> #print
+const isNotWindows = process.platform !== 'win32';
+
+if (isNotWindows) {
+  it('No args', async () => {
+    await t(
+      conf,
+      'print',
+      `>> #print
 >> node ./tests/data/args.js
 "[]"`,
-    { dz: true },
-  );
-});
+      { dz: true },
+    );
+  });
 
-it('Args', async () => {
-  await t(
-    conf,
-    'print -a "  a cc " --e',
-    `>> #print
+  it('Args', async () => {
+    await t(
+      conf,
+      'print -a "  a cc " --e',
+      `>> #print
 >> node ./tests/data/args.js -a "  a cc " --e
 "["-a","  a cc ","--e"]"`,
-    { dz: true },
-  );
-});
+      { dz: true },
+    );
+  });
+}
