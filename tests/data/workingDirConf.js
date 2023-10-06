eabb25397cf07a869b9070b3d8f69bbed927cf9a
Mgen <m@mgenware.com>
2023-10-07 04:54:42 +0800

    Fix windows __dirname starting with /

--- tests/data/workingDirConf.js
+++ tests/data/workingDirConf.js
@@ -1,13 +1,18 @@
+import { platform } from 'os';
 import * as np from 'path';
 import { URL } from 'url';
 
-const __dirname = new URL('.', import.meta.url).pathname;
+let __dirname = new URL('.', import.meta.url).pathname;
+// Fix windows path starting with `/`.
+if (platform() === 'win32' && __dirname.startsWith('/')) {
+  __dirname = __dirname.slice(1);
+}
 
 export default {
   log: {
     // This command will be executed with different working directories.
     // Use `__dirname` to find `curDirName.js` in different working directories.
-    run: [`node ${np.join(__dirname, 'helper/curDirName.js')}`],
+    run: [`node ${np.resolve(__dirname, 'helper/curDirName.js')}`],
   },
   child: {
     run: '#log',
